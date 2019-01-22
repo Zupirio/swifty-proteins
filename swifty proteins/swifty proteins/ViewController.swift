0c804cf7114ca525dfd250de1781cd6d618bec8a
Zupirio <28960804+Zupirio@users.noreply.github.com>
2019-01-22 14:10:43 +0200

    first commit

---
+++ swifty proteins/swifty proteins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swifty proteins
+//
+//  Created by Asahel RANGARIRA on 2019/01/22.
+//  Copyright © 2019 Asahel RANGARIRA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
