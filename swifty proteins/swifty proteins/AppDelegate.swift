3d1cfa8a4d75b5fa88e588e296139a2cebd9f1e3
Zupirio <zupirio@gmail.com>
2019-01-24 09:31:04 +0200

    touchId Button

--- swifty proteins/swifty proteins/AppDelegate.swift
+++ swifty proteins/swifty proteins/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-		Switcher.updateRootVC()
-		sleep(2);
+//		Switcher.updateRootVC()
+//		sleep(2);
         return true
     }
 
--- swifty proteins/swifty proteins/LoginViewController.swift
+++ swifty proteins/swifty proteins/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
 
@@ -16,18 +17,56 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-	@IBAction func logout(_ sender: Any) {
-		UserDefaults.standard.set(false, forKey: "status")
-		Switcher.updateRootVC()
+//	@IBAction func logout(_ sender: Any) {
+//		UserDefaults.standard.set(false, forKey: "status")
+//		Switcher.updateRootVC()
+//	}
+	
+	@IBOutlet var successLabel: UILabel!
+	
+	
+	@IBAction func touchIdAction(_ sender: UIButton) {
+		
+		print("hello there!.. You have clicked the touch ID")
+		
+		let myContext = LAContext()
+		let myLocalizedReasonString = "Biometric Authntication testing !! "
+		
+		var authError: NSError?
+		if #available(iOS 8.0, macOS 10.12.1, *) {
+			if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+				myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+					
+					DispatchQueue.main.async {
+						if success {
+							// User authenticated successfully, take appropriate action
+							self.successLabel.text = "Awesome!!... User authenticated successfully"
+						} else {
+							// User did not authenticate successfully, look at error and take appropriate action
+							self.successLabel.text = "Sorry!!... User did not authenticate successfully"
+						}
+					}
+				}
+			} else {
+				// Could not evaluate policy; look at authError and present an appropriate message to user
+				successLabel.text = "Sorry!!.. Could not evaluate policy."
+			}
+		} else {
+			// Fallback on earlier versions
+			
+			successLabel.text = "Ooops!!.. This feature is not supported."
+		}
+		
+		
 	}
-    /*
+}
+	
+	/*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // In a storyboard-based application, you will often want to do a little preparation before navigation*/
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
 
-}
